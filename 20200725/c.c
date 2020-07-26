49bb0d90eda7a79ec58d44654c919663215204f9
shunya-s15 <shunya.suraimu@gmail.com>
2020-07-26 20:43:30 +0900

    プログラムが強制終了する

--- 20200725/c.c
+++ 20200725/c.c
@@ -1,33 +1,18 @@
 #include<stdio.h>
 typedef long long ll;
-ll a[300000];
-ll totalpoint(ll n,ll k){
-    ll i,ans;
-    ans = 1;
-    for(i=n-k+1;i<=n;i++){
-        ans = ans*a[i];
-    }
-    return ans;
-}
 int main(){
-    ll N,K;
-    ll i,point,atopoint;
+    ll N,K,i;
+    ll A[300000];
     scanf("%lld%lld",&N,&K);
     for(i=1;i<=N;i++){
-        scanf("%lld",&a[i]);
+        scanf("%lld",&A[i]);
     }
-    point = totalpoint(K,K);
-    for(i = K+1;i<=N;i++){
-        atopoint = (point/a[i-K])*a[i];
-        //printf("%lld",point);
-        if(point<atopoint){
-            printf("Yes");
+    for(i=1;i<=N;i++){
+        if(A[i]<A[K+i]){
+            printf("Yes\n");
         }else{
-            printf("No");
+            printf("No\n");
         }
-        printf("\n");
-        point = atopoint;
     }
-    //printf("%lld",atopoint);
     return 0;
 }
