1404d82a8f31f136ac9cf220323915d74e17510c
shunya-s15 <shunya.suraimu@gmail.com>
2020-12-13 07:57:19 +0900

    なんかわからんけど問題起きてた

--- 20201024/b.cpp
+++ 20201024/b.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 int main(){
-    int n;
-    cin >> n;
+    int n,m;
+    cin >> n >> m;
+    vector<int> a(n);
+    vector<int> b(n);
+    vector<pair<int,int>> cd(m);
+    for(int i=0;i<n;i++){
+        cin >> a.at(i);
+    }
+    for(int i=0;i<n;i++){
+        cin >> b.at(i);
+    }
+    for(int i=0;i<m;i++){
+        cin >> cd.at(i).first << cd.at(i).second;
+    }
+    
 }
